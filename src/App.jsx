b20f6ff94f743b3703870e6e236de07af77befba
OleksiyVoloshyn <gromov196312@gmail.com>
2025-01-18 19:22:57 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Description from './components/Description/Description';
 import Options from './components/Options/Options';
-import Feedback from './components/Feedback/Feedback';
+import Feedback from './components/Feedback /Feedback';
 import Notification from './components/Notification/Notification';
 
 const DEFAULT_FEEDBACK_DATA = {
